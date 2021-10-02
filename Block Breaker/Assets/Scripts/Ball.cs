6da091eaed61ac9b453dc20a3d2a28e0fb8ff480
David Every <David_every.ovr@hotmail.com>
2021-10-02 01:48:45 +0100

    Initilizing Block breaker

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	void Start(){
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector =  this.transform.position - paddle.transform.position;
+		
+	}
+	
+	void Update(){
+		if (!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		
+			if (Input.GetMouseButtonDown(0)) {
+				//print ("Launch Ball");
+				hasStarted =  true;
+				this.rigidbody2D.velocity = new Vector2 (2f,10f);
+			}
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+	
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+	
+		if (hasStarted) {
+			audio.Play();
+			rigidbody2D.velocity += tweak;
+		}
+	}
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public bool autoPlay = false;
+	public float minX, maxX;
+	private Ball ball;
+	
+	//Screen.showCursor=false says that once the player has clicked to launch the ball
+	//the mouse cursor will vanish from view. this will return upon game loss
+	void Start() {
+		Screen.showCursor = false; 
+		ball = GameObject.FindObjectOfType<Ball>();
+		
+	}
+	
+	// Update is called once per frame
+	//this means that every frame the code will check to see where the players mouse is
+	//and will move the Paddle accordingly
+	void Update () {
+		if (!autoPlay) {
+			MoveWithMouse();
+		}else{
+			AutoPlay();
+		}
+	}
+	// The game will play its self using this code
+	void AutoPlay() {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		Vector3 ballPos = ball.transform.position;
+		paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
+		this.transform.position = paddlePos;
+	
+	}
+	//The player will control the Paddle using their mouse to move left or right
+	void MoveWithMouse () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width *16;
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
+		this.transform.position = paddlePos;
+	}
+}
+
+
+
+
+
+
